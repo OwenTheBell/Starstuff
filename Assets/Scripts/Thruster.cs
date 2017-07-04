9ae80701261b596a5c613a0d664b9f7179f9467c
Owen Bell <owenbellgames@gmail.com>
2017-07-04 17:56:50 -0400

    Added max velocity to the thrusters

--- Assets/Scripts/Thruster.cs
+++ Assets/Scripts/Thruster.cs
@@ -2,35 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Thruster : MonoBehaviour
-{
+public class Thruster : MonoBehaviour {
 
 	public KeyCode ThrustKey;
 	public float Force;
-
-	void Start ()
-	{
-		
-	}
+    public bool HasMaxVelcity;
+    public float MaxVelocity;
 	
-	void FixedUpdate ()
-	{
-		if (Input.GetKey(ThrustKey))
-		{
-			var force = transform.up * Force;
-			GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Force);
-		}
-
-		if (gameObject.HasComponentInChildren<ParticleSystem>())
-		{
-			if (Input.GetKeyDown(ThrustKey))
-			{
+	void FixedUpdate () {
+		if (gameObject.HasComponentInChildren<ParticleSystem>()) {
+			if (Input.GetKeyDown(ThrustKey)) {
 				GetComponentInChildren<ParticleSystem>().Play();
 			}
-			else if (Input.GetKeyUp(ThrustKey))
-			{
+			else if (Input.GetKeyUp(ThrustKey)) {
 				GetComponentInChildren<ParticleSystem>().Stop();
 			}
 		}
+
+        var rigidbody = GetComponent<Rigidbody2D>();
+        if (HasMaxVelcity && rigidbody.velocity.magnitude >= MaxVelocity) {
+            return;
+        }
+		if (Input.GetKey(ThrustKey)) {
+			var force = transform.up * Force;
+			rigidbody.AddForce(force, ForceMode2D.Force);
+		}
+
 	}
 }
